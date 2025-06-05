c9df9c826ea4934a5e04e61612ee76bccdbea011
jeli01 <jeli01@naver.com>
2025-06-06 01:45:31 +0900

    feat: chat-relay 기능 구현 완료

--- src/main/java/kakaotech/bootcamp/respec/specranking/chatconsumer/domain/chatparticipation/repository/ChatParticipationRepository.java
+++ src/main/java/kakaotech/bootcamp/respec/specranking/chatconsumer/domain/chatparticipation/repository/ChatParticipationRepository.java
@@ -2,6 +2,9 @@ package kakaotech.bootcamp.respec.specranking.chatconsumer.domain.chatparticipat
 
 import kakaotech.bootcamp.respec.specranking.chatconsumer.domain.chatparticipation.entity.ChatParticipation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ChatParticipationRepository extends JpaRepository<ChatParticipation, Long> {
+
 }
--- src/main/java/kakaotech/bootcamp/respec/specranking/chatconsumer/domain/chatroom/entity/Chatroom.java
+++ src/main/java/kakaotech/bootcamp/respec/specranking/chatconsumer/domain/chatroom/entity/Chatroom.java
@@ -6,15 +6,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import kakaotech.bootcamp.respec.specranking.chatconsumer.domain.common.BaseTimeEntity;
-import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Chatroom extends BaseTimeEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(columnDefinition = "BIGINT UNSIGNED")
